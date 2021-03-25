19cdacf6a1727e9909d1909d0b4a861692d432bb
mijung-park <mezzang.park@gmail.com>
2021-03-25 10:28:23 +0900

    first commint

---
+++ src/p01/Comment.java
@@ -0,0 +1,20 @@
+package p01;
+
+public class Comment {
+	public static void main(String arg[]) {
+		System.out.println("주석");
+
+		// 한줄 주석은 slash 두개
+		System.out.println("한 줄 주석");
+
+		/*
+		 * 
+		 * 여러 줄 주석은 slash 별 ~ 별 slash까지
+		 * 
+		 */
+		System.out.println("여러 줄 주석");
+		
+		//sout 단축키로 System.out.println 출력가능
+
+	}
+}
