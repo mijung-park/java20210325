c67c0f3c4f74a0a2d41fd3a1d88191270868f6e5
mijung-park <mezzang.park@gmail.com>
2021-04-05 17:23:45 +0900

    chapter 05

--- src/p05/textbook/exercise/Exercise07.java
+++ src/p05/textbook/exercise/Exercise07.java
@@ -4,23 +4,24 @@ public class Exercise07 {
 	public static void main(String[] args) {
 		int max = Integer.MIN_VALUE;
 		int min = Integer.MAX_VALUE;
-		int sum = 0;
-		double avg = 0.0;
 		
 		int[] array = {1, 5, 3, 8, 2};
-		
 		for (int i = 0; i < array.length; i++) {
 			if (max < array[i]) {
 				max = array[i];
 			}
-		} 	System.out.println("max: " + max);
-		
+		}
+		System.out.println("max: " + max);
 		
 		for (int i = 0; i < array.length; i++) {
 			if (min > array[i]) {
 				min = array[i];
 			}
-		} 	System.out.println("min: " + min);
+		}
+		System.out.println("min: " + min);
+		
+		int sum = 0;
+		double avg = 0;
 		
 		for (int i = 0; i < array.length; i++) {
 			sum += array[i];
