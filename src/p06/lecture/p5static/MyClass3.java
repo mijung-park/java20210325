f6d91846817ca3655bffe3222b0c9c649410ca71
mijung-park <mezzang.park@gmail.com>
2021-04-08 14:23:38 +0900

    chapter 06

--- src/p06/lecture/p5static/MyClass3.java
+++ src/p06/lecture/p5static/MyClass3.java
@@ -1,5 +1,32 @@
 package p06.lecture.p5static;
 
 public class MyClass3 {
+	
+	int i;
+	static int j;
 
+	static void method1() {
+		System.out.println("method1");
+		method3();
+//		this.method3(); this 키워드로 호출불가! instance멤버만 사용가능
+		System.out.println(j);
+//		System.out.println(i);
+	}
+	
+	static void method3() {
+		System.out.println("method3");
+//		method2(); static멤버에서 instance멤버에 접근 불가능
+	}
+	
+	void method2() {
+		System.out.println("method2");
+		this.method4();
+		
+		// instance멤버는 static멤버에 접근가능
+		method3();
+	}
+	
+	void method4() {
+		System.out.println("method4");
+	}
 }
