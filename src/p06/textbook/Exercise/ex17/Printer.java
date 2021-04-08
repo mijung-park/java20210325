2e927c4c8faaccb85d84d5aec3566aa6bb258f73
mijung-park <mezzang.park@gmail.com>
2021-04-08 14:04:41 +0900

    chapter 06

---
+++ src/p06/textbook/Exercise/ex17/Printer.java
@@ -0,0 +1,26 @@
+package p06.textbook.Exercise.ex17;
+
+public class Printer {
+
+	static void println(int a) {
+		System.out.println(a);
+	}
+	
+	static void println(boolean a) {
+		System.out.println(a);
+	}
+	
+	static void println(double a) {
+		System.out.println(a);
+	}
+	
+	static void println(String a) {
+		System.out.println(a);
+	}
+
+	static void println(String a, String b) {
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
---
+++ src/p06/textbook/Exercise/ex17/PrinterExample.java
@@ -0,0 +1,12 @@
+package p06.textbook.Exercise.ex17;
+
+public class PrinterExample {
+	public static void main(String[] args) {
+		
+		Printer.println(10);
+		Printer.println(true);
+		Printer.println(5.7);
+		Printer.println("홍길동");
+		Printer.println("may", "heesoo");
+	}
+}
