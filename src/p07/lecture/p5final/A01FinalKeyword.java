f1f850760d2aa98f64fafca8878fe3e9ca19e4d1
mijung-park <mezzang.park@gmail.com>
2021-04-12 14:09:30 +0900

    chapter 07

---
+++ src/p07/lecture/p5final/A01FinalKeyword.java
@@ -0,0 +1,6 @@
+package p07.lecture.p5final;
+
+public class A01FinalKeyword {
+	// final class: 상속하지 못하는 클래스
+	// final method: override하지 못하는 클래스
+}
---
+++ src/p07/lecture/p5final/MyString.java
@@ -0,0 +1,5 @@
+package p07.lecture.p5final;
+
+public class MyString extends String {
+	// String은 상속할 수 없는 final !
+}
