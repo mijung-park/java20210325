0ebea2ad426c5039d84711a40c0184dfcc96180f
mijung-park <mezzang.park@gmail.com>
2021-04-12 15:51:28 +0900

    chapter 07

--- src/p07/lecture/p5final/Child.java
+++ src/p07/lecture/p5final/Child.java
@@ -2,4 +2,12 @@ package p07.lecture.p5final;
 
 public final class Child extends Parent {
 
+	@Override
+	public void method1() {
+		System.out.println("method1");
+	}
+	
+//	public void method2() {
+//		System.out.println("method2");
+//	}
 }
