b3e36c9513f54c9f0da259b2bb0bb9815299178f
mijung-park <mezzang.park@gmail.com>
2021-04-13 15:36:24 +0900

    chapter 07

---
+++ src/p07/textbook/s070802/PhoneExample.java
@@ -0,0 +1,14 @@
+package p07.textbook.s070802;
+
+public class PhoneExample {
+	public static void main(String[] args) {
+		// Phone phone = new Phone();
+		
+		SmartPhone smartphone = new SmartPhone("홍길동");
+		
+		smartphone.turnOn();
+		smartphone.internetSearch();
+		smartphone.turnOff();
+				
+	}
+}
