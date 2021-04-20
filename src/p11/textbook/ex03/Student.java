c3555fa17ad21c58ec79f4c9b6e27563e7620c25
mijung-park <mezzang.park@gmail.com>
2021-04-20 17:04:08 +0900

    chapter 11

---
+++ src/p11/textbook/ex03/Student.java
@@ -0,0 +1,24 @@
+package p11.textbook.ex03;
+
+public class Student {
+	private String studentNum;
+	
+	public Student(String studentNum) {
+		this.studentNum = studentNum;
+	}
+	
+	public String getStudentNum() {
+		return studentNum;
+	}
+	
+	@Override
+	public int hashCode () {
+		return studentNum.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		return this.studentNum.equals(((Student) obj).studentNum);
+	}
+}
