5b689eba8260bdd70d40b3ff941659ed003ca680
mijung-park <mezzang.park@gmail.com>
2021-04-21 11:38:03 +0900

    chapter 11

---
+++ src/p11/textbook/ex4/MemberExample.java
@@ -0,0 +1,9 @@
+package p11.textbook.ex4;
+
+public class MemberExample {
+	public static void main(String[] args) {
+		Member member = new Member("blue", "이파란");
+		
+		System.out.println(member);
+	}
+}
